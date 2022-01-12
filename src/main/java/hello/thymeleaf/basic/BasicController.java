15373543137529e34cc827412488c43ffa43b409
Ea-Bell <cjp975@naver.com>
2022-01-12 15:35:20 +0900

    타임리프 -기본 기능
    블록
    
    <th:block>를 쓰면 하나의 영역으로 인식을 한다. 그래서 반복해서 html태그를
    쓸때 쓰면 편하다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -117,6 +117,13 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
     @Component("helloBean")
     static class HelloBean{
         public String hello(String data){
